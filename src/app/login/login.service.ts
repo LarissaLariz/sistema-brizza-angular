b71fa4bf3495deb6a5f95541909ba844c1ef8022
LarissaLariz <larissalariz@hotmail.com>
2026-04-14 21:15:39 -0300

    Configura rota de login

---
+++ src/app/login/login.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  private apiUrl = 'http://localhost:8080/auth/login';
+
+  constructor(private http: HttpClient) {}
+
+  login(email: string, senha: string): Observable<any> {
+    return this.http.post(this.apiUrl, {
+      email: email,
+      senha: senha
+    });
+  }
+}
