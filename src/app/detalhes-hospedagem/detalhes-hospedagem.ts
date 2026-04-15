0ae9a83b9e461cc76e957c11942202f8edcf5653
LarissaLariz <larissalariz@hotmail.com>
2026-04-14 21:14:06 -0300

    Adiciona detalhes e descricao de reservas

--- src/app/detalhes-hospedagem/detalhes-hospedagem.ts
+++ src/app/detalhes-hospedagem/detalhes-hospedagem.ts
@@ -1,20 +1,27 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgFor, NgIf } from '@angular/common';
+import { HOSPEDAGENS } from '../dados/hospedagens';
 
 @Component({
   selector: 'app-detalhes-hospedagem',
   standalone: true,
-  imports: [],
   templateUrl: './detalhes-hospedagem.html',
   styleUrl: './detalhes-hospedagem.css',
 })
 export class DetalhesHospedagem implements OnInit {
   private route = inject(ActivatedRoute);
+  private router = inject(Router);
+  
+  hospedagemSelecionada: any = null;
+  Voltar() {
+    this.router.navigate(['/home']);}
   id: number | null = null;
 
   ngOnInit() {
     const idRecebido = this.route.snapshot.paramMap.get('id');
     this.id = Number(idRecebido);
+    this.hospedagemSelecionada = HOSPEDAGENS.find((hospedagem) => hospedagem.id === this.id);
     console.log('ID recebido:', this.id);
   }
 }
