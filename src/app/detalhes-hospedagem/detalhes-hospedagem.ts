489764fb32e4ef994a68f68e0d158b85db5a8464
larissasouzaa <271653995+LarissaLariz@users.noreply.github.com>
2026-04-17 14:19:22 -0300

    Implementa lógica da tela de detalhes da hospedagem

--- src/app/detalhes-hospedagem/detalhes-hospedagem.ts
+++ src/app/detalhes-hospedagem/detalhes-hospedagem.ts
@@ -1,20 +1,27 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { NgFor, NgIf } from '@angular/common';
+import { HOSPEDAGENS } from '../dados/hospedagens';
 
 @Component({
   selector: 'app-detalhes-hospedagem',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './detalhes-hospedagem.html',
   styleUrl: './detalhes-hospedagem.css',
 })
 export class DetalhesHospedagem implements OnInit {
   private route = inject(ActivatedRoute);
+  
+  hospedagemSelecionada: any = null;
+  Voltar() {
+    this.route.navigate(['/home']);}
   id: number | null = null;
 
   ngOnInit() {
     const idRecebido = this.route.snapshot.paramMap.get('id');
     this.id = Number(idRecebido);
+    this.hospedagemSelecionada = HOSPEDAGENS.find((hospedagem) => hospedagem.id === this.id);
     console.log('ID recebido:', this.id);
   }
 }
